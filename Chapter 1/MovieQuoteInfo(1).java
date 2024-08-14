1796fae0a3e8efe9b80f2d96645738f43aad37b3
JustinJohnDomagtoy <justinjohndomagtoy@gmail.com>
2024-08-14 11:19:05 -0400

    Add files via upload

---
+++ Chapter 1/MovieQuoteInfo(1).java
@@ -0,0 +1,13 @@
+public class MovieQuoteInfo {
+    public static void main(String[] args) {
+        String quote = "One shall stand and One shall fall";
+        String movie = "Transformers";
+        String character = "Optimus Prime";
+        int year = 2007;
+
+        System.out.println("Quote: \"" + quote + "\"");
+        System.out.println("Movie: " + movie);
+        System.out.println("Character: " + character);
+        System.out.println("Year: " + year);
+    }
+}
