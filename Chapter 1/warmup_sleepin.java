15a3316788e3c08cf731565dba95a3eb57470b11
JustinJohnDomagtoy <justinjohndomagtoy@gmail.com>
2024-08-29 09:39:36 -0400

    Add files via upload

---
+++ Chapter 1/warmup_sleepin.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Main obj = new Main();
+
+        System.out.println(obj.sleepIn(false, false));  
+        System.out.println(obj.sleepIn(true, false));   
+        System.out.println(obj.sleepIn(false, true));   
+    }
+
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        return !weekday || vacation;
+    }
+}
