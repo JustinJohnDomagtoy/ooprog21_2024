338e8f9dae2d93d4057429f845ef72291383bde8
JustinJohnDomagtoy <justinjohndomagtoy@gmail.com>
2024-08-30 07:44:45 +0700

    Update MovieQuoteInfo(1).java

--- Chapter 1/MovieQuoteInfo(1).java
+++ Chapter 1/MovieQuoteInfo(1).java
@@ -5,9 +5,5 @@ public class MovieQuoteInfo {
         String character = "Optimus Prime";
         int year = 2007;
 
-        System.out.println("Quote: \"" + quote + "\"");
-        System.out.println("Movie: " + movie);
-        System.out.println("Character: " + character);
-        System.out.println("Year: " + year);
     }
 }
