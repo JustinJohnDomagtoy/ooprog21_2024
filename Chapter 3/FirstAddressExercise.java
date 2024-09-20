c9b16a8023c2d32603689c4c64b5cd5f42494bc7
JustinJohnDomagtoy <justinjohndomagtoy@gmail.com>
2024-09-20 08:03:48 -0400

    Add files via upload

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,12 @@
+public class FirstAddressExercise {
+    public static void main(String[] args) {
+        displayAddress();
+    }
+
+    public static void displayAddress() {
+        System.out.println("Tecno");
+        System.out.println("No. 18 Transsion Avenue");
+        System.out.println("Shenzhen, Guangdong, China");
+        System.out.println("Company: Tecno Mobile Inc.");
+    }
+}
